f98bf613ab64b4ce6e0a15e5c5a24247853230ca
Vika Strigo <vika120519841330@gmail.com>
2019-05-15 18:17:41 +0300

    добавлены  Lesson_3_Task_main_1, Lesson_3_Task_main_2
    добавлены Lesson_3_Task_main_1,
    Lesson_3_Task_main_2

---
+++ Lesson_3/Lesson_3_Home_Task_Main_1/Task_Main_1_for.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lesson_3_Home_Task_Main_1
+{
+    class Program
+    /*Ввести с клавиатуры число n большее 0
+    Вывести на экран все числа из диапазона от 0 до n, которые являются простыми.
+    Продемонстрировать использование операторов for, break, continue.*/
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            Console.WriteLine("Введите любое натуральное (положительное целое) число:");
+            try { 
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Веденное число не соответствует требованию: ЦЕЛОЕ ЧИСЛО");
+                Console.WriteLine("Попробуйте ввести число еще раз!");
+            }
+            if (num <= 0)
+            {
+                Console.WriteLine("Веденное число не соответствует требованию: ПОЛОЖИТЕЛЬНОЕ ЧИСЛО");
+                Console.WriteLine("Попробуйте ввести число еще раз!");
+                return;//выход из метода Main при неудачной попытке ввести натуральное число
+            }
+            bool isSimpleNum = true;
+            int i;
+            int div;
+            for (i = 2; i <= num; i++)
+            {
+                for (div = 2; div < i; div++)
+                    if (i % div == 0)
+                    {
+                        isSimpleNum = false;
+                        break;
+                    }
+                    else continue;
+                if ((isSimpleNum == true)||(i == div))
+                    Console.Write($"{i}  ");
+            }
+        }
+    }
+}
---
+++ Lesson_3/Lesson_3_Task_Main/Task_Main_2_while.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Lesson_3_Task_Main
+{
+    class MyException : Exception {
+        public MyException(string str) : base(str) { }
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+    class SimpleNum
+        /*Ввести с клавиатуры число n большее 0
+        Вывести на экран все числа из диапазона от 0 до n, которые являются простыми.
+        Продемонстрировать использование цикла while.*/
+    {
+        int num;
+        public SimpleNum()
+        {
+            //конструктор по умолчанию
+        }
+        public int Number
+        {
+            get
+            {
+                return num;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new MyException("Веденное число не соответствует требованию: ПОЛОЖИТЕЛЬНОЕ ЧИСЛО");
+                else num = value;
+            }
+        }
+        public bool IsSimpleNum(int k)
+        {
+            int n = k;
+            bool flag = true;
+            for (int j = 2; (j <= n) && (flag); j++)
+            {
+                if ((n % j == 0)&&(j != n))
+                {
+                    flag = false;
+                    return flag;
+                }     
+            }
+            return flag;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите любое положительное целое число:");
+                SimpleNum obj_1 = new SimpleNum();
+                try
+                { 
+                obj_1.Number = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Веденное число не соответствует требованию: ЦЕЛОЕ ЧИСЛО");
+                } 
+                int i = 2;
+                while (i <= obj_1.Number)
+                    {
+                    if (obj_1.IsSimpleNum(i))
+                        Console.Write(i + " ");
+                    i++;
+                    }
+            }catch (MyException exc)
+            {
+                Console.WriteLine(exc);
+                Console.WriteLine("Попробуйте ввести число еще раз!");
+            }
+        }
+    }
+}
