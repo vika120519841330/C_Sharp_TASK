d29b6d0542238f751a056ea943f5a70a41eb9119
Vika Strigo <vika120519841330@gmail.com>
2019-05-30 00:01:03 +0300

    Lesson 7 Player
    Lesson 7 Player

--- Lesson_6/Lesson_6_HomeTask_main/Program.cs
+++ Lesson_6/Lesson_6_HomeTask_main/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 
-namespace Lesson_6_HomeTask_main
+namespace Lesson_06_HomeTask_main
 {
     class PlayerDemo
     {
         static void Main(string[] args)
         {
+            
             // Использование членов класса Player
             Player play_A = new Player();
             play_A.Ram = 128;
@@ -24,27 +25,55 @@ namespace Lesson_6_HomeTask_main
             Console.WriteLine();
 
             //Использование членов класса Album
-            Album albA = new Album ();
-            string[] listSinger = albA.FillAlbumSinger();
+            Album albA = new Album();
+
             Console.WriteLine("Список исполнителей:\t");
-            foreach (string s in listSinger)
+            foreach (string s in albA.singer)
                 Console.WriteLine($"-> {s}");
             Console.WriteLine();
 
-            string[] listSong = albA.FillAlbumSong();
             Console.WriteLine("Список песен:\t");
-            foreach (string s in listSong)
+            foreach (string s in albA.song)
                 Console.WriteLine($" -> {s}");
             Console.WriteLine("\n");
 
             albA.CollectionDictionary();
 
             Console.WriteLine($"\nИсполнитель песни <<Ласточки>>:  {albA["Ласточки"]}");
-            Console.WriteLine($"\nЛяпис Трубецкой исполняет песню:  {albA[0]}");
-            Console.WriteLine($"\nДемонстрация выхода за пределы индексирования:  {albA[5]}");
-            Console.WriteLine($"\nДемонстрация выхода за пределы индексирования:  {albA[-2]}");
-            Console.WriteLine($"\nДемонстрация обращения к несуществующему элемента массива:  {albA["Аривидерчи"]}");
-        }
+            Console.WriteLine($"Ляпис Трубецкой исполняет песню:  {albA[0]}");
+            Console.WriteLine($"\nДемонстрация выхода за пределы индексирования:");
+            Console.Write($"{albA[5]}");
+            Console.Write($"{albA[-2]}");
+            Console.WriteLine($"\nДемонстрация обращения к несуществующему элементу массива:");
+            Console.WriteLine($"{albA["Аривидерчи"]}");
+
+            Console.WriteLine($"\nДемонстрация применения сортировки списка исполнителей и списка песен:\n");
+            Album albB = new Album();
+            albB.singer.SortStrings(); // Вызов статического метода расширения(сортировка массивас исполнителями) способом №1
+            StaticClass.SortStrings(albB.song); // Вызов статического метода расширения(сортировка массива с песнями) способом №2
+            Console.WriteLine();
+
+            Console.WriteLine($"\nДемонстрация добавления  НОВОГО элемента в альбом с исполнителями и песнями:");
+            Album albC = new Album();
+            string[] temp1 = new string [albC.singer.Length]; //временный массив для выхова метода расширения НА СЕБЯ
+
+            temp1.Add(ref albC.singer, ref albC.song, "Михаил Круг", "Владимирский централ"); // Вызов статического метода расширения способом №1
+            Console.WriteLine($"Длина обновленного расширенного массива с композициями равна: {albC.singer.Length}");
+            Console.WriteLine($"Длина обновленного расширенного массива с исполнителями равна: {albC.song.Length}");
+            Console.WriteLine();
+
+            temp1 = new string[albC.singer.Length];
+
+            StaticClass.Add(temp1,ref albC.singer, ref albC.song, "Ночные снайперы", "31 весна"); // Вызов статического метода расширения способом №2
+            Console.WriteLine($"Длина обновленного расширенного массива с композициями равна: {albC.singer.Length}");
+            Console.WriteLine($"Длина обновленного расширенного массива с исполнителями равна: {albC.song.Length}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Демонстрация обращения к ранее существовавшему элементу альбома ПО ИНДЕКСУ: {albC[1]}");
+            Console.WriteLine($"Демонстрация обращения к только что добавленному НОВОМУ элементу альбома ПО ИНДЕКСУ: {albC[6]}");
+            Console.WriteLine($"Демонстрация обращения к ранее существовавшему элементу альбома ПО СТРОКЕ: {albC["Бежать"]}");
+            Console.WriteLine($"Демонстрация обращения к только что добавленному НОВОМУ элементу альбома ПО СТРОКЕ: {albC["Владимирский централ"]}");
+        }    
     }
     class Player
     {
@@ -99,10 +128,22 @@ namespace Lesson_6_HomeTask_main
     }
     class Album 
     {
-        string[] singer; //обьявление массива, содержащего список исполнителей
-        string[] song; //обьявление массива, содержащего список песен
+        public string[] singer; //обьявление массива, содержащего список исполнителей
+        public string[] song; //обьявление массива, содержащего список песен
 
-        //перегрузка индексаторов
+        public Album()
+        {
+            singer = this.FillAlbumSinger();
+            song = this.FillAlbumSong();
+        }
+        // Перегрузка конструктора
+        public Album(string []si, string[]so)
+        {
+            singer = si;
+            song = so;
+        }
+
+        //Перегрузка индексаторов
         public string this[int index] //индексатор для доступа к массиву с песнями ПО ЧИСЛУ
         {
             get
@@ -133,6 +174,8 @@ namespace Lesson_6_HomeTask_main
                         case "Alejandro": return singer[2]; break;
                         case "Ода нашей любви": return singer[3]; break;
                         case "Одинокая птица": return singer[4]; break;
+                        case "Владимирский централ": return singer[5]; break;
+                        case "31 весна": return singer[6]; break;
                         default:
                             Console.WriteLine("Такой песни нет в альбоме!!");
                             return null;
@@ -145,14 +188,14 @@ namespace Lesson_6_HomeTask_main
                 }     
             }
         }
-        // Метод для инициализации массива содержащего список исполнителей
-        public string[] FillAlbumSinger()
+        // Закрытый метод для инициализации массива содержащего список исполнителей
+        private string[] FillAlbumSinger()
         {
             this.singer = new string[5] { "Ляпис Трубецкой", "Аня Шаркунова", "Lady Gaga", "Т9", "Вячеслав Бутусов" };
             return this.singer;
         }
-        // Метод для инициализации массива содержащего список песен
-        public string[] FillAlbumSong()
+        // Закрытый метод для инициализации массива содержащего список песен
+        private string[] FillAlbumSong()
         {
             this.song = new string[5] { "Ласточки", "Бежать", "Alejandro", "Ода нашей любви", "Одинокая птица" };
             return this.song;
@@ -165,6 +208,46 @@ namespace Lesson_6_HomeTask_main
             {
                 Console.WriteLine($"Исполнитель: {this[this[i]]} \t  -------> Песня: {this[i]}");
             } 
+        }
+    }
+    // Статический класс с методами расширения
+    public static class StaticClass 
+    {
+        //Метод расширения для сортировки массива строк string[]
+        public static void SortStrings(this string[] arr)
+        {
+            Console.WriteLine($"Массив строк ДО сортировки:");
+            foreach (string s in arr)
+                Console.Write($"{s}\0");
+            Console.WriteLine();
+
+            Array.Sort(arr);
+
+            Console.WriteLine($"Массив строк ПОСЛЕ сортировки:");
+            foreach (string s in arr)
+                Console.Write($"{s}\0");
+            Console.WriteLine();
+        }
+        // Метод расширения для добавления в коллекцию типа Album новых элементов
+        public static void Add(this string[]temp, ref string[] singer, ref string[] song, string si, string so)
+        {
+            int lenghtInitial = temp.Length; //локальная переменная хранящая первоначальную длину списка исполнителей/песен
+
+            //Расширение списка исполнителей на одну единицу
+            Array.Resize(ref singer, singer.Length + 1);
+            singer[lenghtInitial] = si;
+            Console.WriteLine($"Список исполнителей ПОСЛЕ добавления еще одного исполнителя:");
+            for (int i = 0; i < singer.Length; ++i)
+                Console.WriteLine($"{i}--->{singer[i]}");
+            Console.WriteLine();
+
+            //Расширение списка песен на одну единицу
+            Array.Resize(ref song, song.Length + 1);
+            song[lenghtInitial] = so;
+            Console.WriteLine($"Список песен ПОСЛЕ добавления еще одной композиции:");
+            for (int i = 0; i < song.Length; ++i)
+                Console.WriteLine($"{i}--->{song[i]}");
+            Console.WriteLine();
         }
     }
 }
