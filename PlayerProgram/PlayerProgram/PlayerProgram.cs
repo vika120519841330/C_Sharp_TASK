3f121ed9b181561c38ec4770e694d0afaafafb00
Vika Strigo <vika120519841330@gmail.com>
2019-06-04 16:30:34 +0300

    lesson 8 and pleer
    lesson 8 and pleer

---
+++ PlayerProgram/PlayerProgram/PlayerProgram.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Player
+{
+    class PlayerApply
+    {
+        static void Main(string[] args)
+        {
+            // Использование членов класса Player
+            Player play_A = new Player();
+            play_A.Ram = 128;
+            Console.WriteLine($"Значение поля Ram для play_A: {play_A.Ram}");
+            Console.WriteLine($"Значение поля Producer для play_A: {play_A.Producer}");
+            Console.WriteLine();
+
+            Player play_B = new Player(256);
+            Console.WriteLine($"Значение поля Ram для play_B: {play_B.Ram}");
+            Console.WriteLine($"Значение поля Producer для play_B: {play_B.Producer}");
+            Console.WriteLine();
+
+            Player play_С = new Player("ASUS", 512);
+            Console.WriteLine($"Значение поля Ram для play_С: {play_С.Ram}");
+            Console.WriteLine($"Значение поля Producer для play_С: {play_С.Producer}");
+            Console.WriteLine();
+
+            // Создание  первого пустого (не наполненного треками) альбома
+            Album albA = new Album("Альбом №1");
+            // Создание песни и добавление ее в первый альбом
+            Song song1 = new Song("Песня_2", "Исполнитель_2", 2014);
+            Song song2 = new Song("Песня_1", "Исполнитель_1", 2013);
+            albA.FillAlbum(song1);
+            albA.FillAlbum(song2);
+            // Одновременное создание песни с ее добавлением в первый альбом
+            albA.FillAlbum("Песня_3", "Испольнитель_3", 2015);
+            // Отображение содержимого первого альбома
+            albA.ShowAlb();
+
+            Console.WriteLine();
+
+            // Создание нового второго альбома с одновременным копированием в него всего содержимого первого альбома
+            Album albB = new Album(albA);
+            // Отображение содержимого второго альбома
+            albB.ShowAlb();
+
+            Console.WriteLine();
+
+            //Удаление всего содержимого первого альбома
+            albA.DeliteAlb();
+            // Отображение содержимого первого альбома
+            albA.ShowAlb();
+            Console.WriteLine();
+
+            // Отображение содержимого второго альбома
+            albB.ShowAlb();
+            Console.WriteLine();
+
+            // Создание третьего альбома и наполнение его песнями из другого альбома
+            Album albC = new Album("Альбом №3");
+            albC.FillAlbum(albB["Песня_2"]);
+            albC.FillAlbum(albB[1]);
+            albC.FillAlbum(albB[2]);
+
+            // Отображение содержимого третьего альбома
+            albC.ShowAlb();
+
+            // Сортировка содержимого второго альбома по наименованию песни (методом НЕ РАСШИРЕНИЯ)
+            albB.SortAlb();
+
+            // Сортировка содержимого третьего альбома по наименованию песни (методом РАСШИРЕНИЯ - двумя вариантами вызова)
+            StaticClass.SortAlbom(albC);
+            albC.SortAlbom();
+
+            // Использование расширяющих методов для добавления в альбом песен
+            albC.AddSong(new Song("Песня_5", "Исполнитель_5", 2010));
+            Song song3 = new Song("Песня_4", "Исполнитель_4", 2016);
+            StaticClass.AddSong(albC, song3);
+
+            // Отсортировать обновленный альбом
+            albC.SortAlbom();
+        }
+    }
+    public class Player
+    {
+        readonly string producer;// Поле только для чтения
+        ushort ram;
+
+        public Player() //Конструктор по умолчанию
+        {
+            this.producer = "SONY";
+        }
+        public Player(ushort r) : this() //Пользовательский конструктор №1
+        {
+            this.Ram = r;
+        }
+        public Player(string p, ushort r) //Пользовательский конструктор №2
+        {
+            this.producer = p;
+            this.Ram = r;
+        }
+        public string Producer //Свойство только для чтения из поля
+        {
+            get
+            {
+                return producer;
+            }
+        }
+        public ushort Ram //Свойство для чтения и записи из поля
+        {
+            get
+            {
+                return ram;
+            }
+            set
+            {
+                if (SetRam(value) == true)
+                    ram = value;
+                else
+                {
+                    Console.WriteLine("Объем памяти д.б. равен одному из следующих значений: 16, 32, 64, 128, 256 или 512 Гб!!");
+                    ram = 16; // присвоение значения по умолчанию
+                }
+            }
+        }
+        // Метод, определяющий  правильность задаваемого обьема памяти
+        public bool SetRam(ushort r)
+        {
+            ushort val = r;
+            if (val == 16 || val == 32 || val == 64 || val == 128 || val == 256 || val == 512)
+                return true;
+            else return false;
+        }
+    }
+
+    // Класс, реализующий интерфейс IComparer для сортировки альбома с песнями по наименованию песни
+    class MyComparer : IComparer <Song>
+    {
+        public int Compare(Song s1, Song s2)
+        {
+            return s1.SongTitle.CompareTo(s2.SongTitle);
+
+        }
+    }
+    public class Song
+    {
+        private string songTitle; // закрытое поле Наименование песни
+        private string singerName; // закрытое поле Наименование исполнителя
+        private int songReleaseYear; // закрытое поле Год выпуска песни
+
+        // Пользовательский конструктор
+        public Song(string st, string sn, int sry)
+        {
+            SongTitle = st;
+            SingerName = sn;
+            SongReleaseYear = sry;
+        }
+
+        // Открытые свойства для закрытых полей
+        public string SongTitle { get; set; }
+        public string SingerName { get; set; }
+        public int SongReleaseYear { get; set; }
+    }
+
+    public class Album 
+    {
+        // Закрытое поле наименования альбома
+        private string albumTitle;
+
+        // Открытое свойство, обеспечивающее доступ к наименованию альбома для записи или считывания
+        public string AlbumTitle
+        {
+            get
+            {
+                return albumTitle;
+            }
+            set
+            {
+                albumTitle = value;
+            }
+        }
+        //Закрытая обобщенная коллекция, содержащая список экземпляров класса Song
+        private List<Song> songList = new List<Song>();
+
+        //Открытое свойство, обеспечивающее доступ к закрытой коллекции, содержащей список экземпляров класса Song
+        public List<Song> SongList
+        {
+            get
+            {
+                return songList;
+            }
+            set
+            {
+                songList = value;
+            }
+        }
+
+        // Индексатор для доступа к песне по номеру
+        public Song this[int ind]
+        {
+            get
+            {
+                if (ind >= 0 && ind < SongList.Count)
+                    return SongList[ind];
+                else
+                {
+                    Console.WriteLine($"Номер трека должен находится в пределах от 0 до {SongList.Count-1}");
+                    return null;
+                }
+            }
+            set
+            {
+                if ( (ind >= 0) && (ind < SongList.Count) )
+                    SongList[ind] = value;
+                else
+                {
+                    Console.WriteLine($"Номер трека должен находится в пределах от 0 до {SongList.Count-1}");
+                    SongList[ind] = null;
+                }
+            }
+        }
+
+        // Индексатор для доступа к содержимому альбома по названию песни
+        public Song this[string ind]
+        {
+            get
+            {
+                for (int i = 0; i < SongList.Count; i++)
+                {
+                    if (SongList[i].SongTitle == ind)
+                        return SongList[i];  
+                }
+                Console.WriteLine($"В альбоме нет трека с таким наименованием!!");
+                return null;
+            }
+            set
+            {
+                int i;
+                for (i = 0; i < SongList.Count; i++)
+                {
+                    if (SongList[i].SongTitle == ind)
+                    {
+                        SongList[i] = value;
+                        return;
+                    }      
+                }
+                Console.WriteLine($"В альбоме нет трека с таким наименованием!!");
+                SongList[i] = null;
+            }
+        }
+
+        // Конструктор по умолчанию
+        public Album() 
+        {
+            AlbumTitle = "Неизвестный альбом";
+
+        }
+        // Пользовательский конструктор №1
+        public Album(string at)
+        {
+            AlbumTitle = at;
+        }
+        // Пользовательский конструктор №2
+        public Album(Album anotherAlb) : this()
+        {
+            this.SongList.AddRange(anotherAlb.SongList);
+        }
+        // Пользовательский конструктор №3
+        public Album(Album anotherAlb, string at) : this (at)
+        {
+            this.SongList.AddRange(anotherAlb.SongList);
+        }
+
+
+        // Закрытый метод для наполнения альбома песнями (вариант №1)
+        public void FillAlbum(string st, string sn, int sry)
+        {
+            this.SongList.Add(new Song(st, sn, sry));
+        }
+        // Закрытый метод для наполнения альбома песнями (вариант №2)
+        public void FillAlbum(Song s)
+        {
+            this.SongList.Add(s);
+        }
+        // Закрытый метод для наполнения альбома песнями (вариант №3 - в конец существующего альбома добавляет новую коллекцию )
+        private void FillAlbum(List<Song> otherList)
+        {
+            this.SongList.InsertRange(this.SongList.Count-1, otherList);
+        }
+
+        // Метод для отображения содержимого альбома
+        public void ShowAlb()
+        {
+            Console.WriteLine($"Альбом: <<{this.AlbumTitle}>>");
+            if (this.SongList.Count != null)
+            {
+                foreach (Song s in this.SongList)
+                    Console.WriteLine($"Песня:\0<<{s.SongTitle}>>\t\0Исполнитель:\0<<{s.SingerName}>>\t\0Год выпуска песни:\0{s.SongReleaseYear}");
+            }
+            else Console.WriteLine("Альбом пуст!!");
+        }
+
+        // Метод для удаления всего содержимого альбома
+        public void DeliteAlb()
+        {
+            Console.WriteLine($"Вы уверенны, что хотите удалить ВСЁ содержимое альбома {this.AlbumTitle}?" +
+                $"\nНажмите плюс + для продолжения");
+            if (Console.ReadLine() == "+")
+            {
+                Console.WriteLine($"Альбом {this.AlbumTitle} очищен");
+                this.SongList.Clear();
+            }     
+        }
+        
+        // Метод  для сортировки альбома с песнями по наименованию песни
+        public void SortAlb()
+        {
+            Console.WriteLine($"\nСодержимое альбома {this.AlbumTitle} ДО сортировки:");
+            this.ShowAlb();
+            
+            this.SongList.Sort(new MyComparer());
+
+            Console.WriteLine($"\nСодержимое альбома {this.AlbumTitle} ПОСЛЕ сортировки:");
+            this.ShowAlb();
+        }
+    }
+    
+
+    // Статический класс с методами расширения
+    public static class StaticClass
+    {
+        //Метод расширения для сортировки альбома с песнями
+         public static void SortAlbom(this Album alb)
+         {
+              Console.WriteLine($"\nСодержимое альбома ДО сортировки:");
+              alb.ShowAlb();
+
+              alb.SongList.Sort(new MyComparer());
+
+              Console.WriteLine($"\nСодержимое альбома ПОСЛЕ сортировки:");
+              alb.ShowAlb();
+        }
+
+        // Метод расширения для добавления в коллекцию типа Album новых элементов
+        public static void AddSong(this Album alb, Song s)
+        {
+            Console.WriteLine($"\nСодержимое альбома ДО добавления еще одной композиции:");
+            alb.ShowAlb();
+
+            Console.WriteLine();
+
+            alb.SongList.Add(s);
+
+            Console.WriteLine($"\nСодержимое альбома ПОСЛЕ добавления еще одной композиции:");
+            alb.ShowAlb();
+
+            Console.WriteLine();
+        }
+    }
+}
+
+
---
+++ PlayerProgram/oldVariant/OLD.cs
@@ -0,0 +1,282 @@
+﻿using System;
+
+namespace oldVariant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Использование членов класса Player
+            Player play_A = new Player();
+            play_A.Ram = 128;
+            Console.WriteLine($"Значение поля Ram для play_A: {play_A.Ram}");
+            Console.WriteLine($"Значение поля Producer для play_A: {play_A.Producer}");
+            Console.WriteLine();
+
+            Player play_B = new Player(256);
+            Console.WriteLine($"Значение поля Ram для play_B: {play_B.Ram}");
+            Console.WriteLine($"Значение поля Producer для play_B: {play_B.Producer}");
+            Console.WriteLine();
+
+            Player play_С = new Player("ASUS", 512);
+            Console.WriteLine($"Значение поля Ram для play_С: {play_С.Ram}");
+            Console.WriteLine($"Значение поля Producer для play_С: {play_С.Producer}");
+            Console.WriteLine();
+
+            //Использование членов класса Album
+            Album albA = new Album();
+
+            Console.WriteLine("Список исполнителей:\t");
+            foreach (string s in albA.singer)
+                Console.WriteLine($"-> {s}");
+            Console.WriteLine();
+
+            Console.WriteLine("Список песен:\t");
+            foreach (string s in albA.song)
+                Console.WriteLine($" -> {s}");
+            Console.WriteLine("\n");
+
+            albA.CollectionDictionary();
+
+            Console.WriteLine($"\nИсполнитель песни <<Ласточки>>:  {albA["Ласточки"]}");
+            Console.WriteLine($"Ляпис Трубецкой исполняет песню:  {albA[0]}");
+            Console.WriteLine($"\nДемонстрация выхода за пределы индексирования:");
+            Console.Write($"{albA[5]}");
+            Console.Write($"{albA[-2]}");
+            Console.WriteLine($"\nДемонстрация обращения к несуществующему элементу массива:");
+            Console.WriteLine($"{albA["Аривидерчи"]}");
+
+            Console.WriteLine($"\nДемонстрация применения сортировки списка исполнителей и списка песен:\n");
+            Album albB = new Album();
+            albB.singer.SortStrings(); // Вызов статического метода расширения(сортировка массива с исполнителями) способом №1
+            Console.WriteLine("\n");
+            StaticClass.SortStrings(albB.song); // Вызов статического метода расширения(сортировка массива с песнями) способом №2
+            Console.WriteLine();
+
+            Console.WriteLine($"\nДемонстрация добавления  НОВОГО элемента в альбом с исполнителями и песнями:\n");
+            Album albC = new Album();
+
+            string[] temp1 = new string[albC.singer.Length]; //Временный массив для вызова метода расширения НА СЕБЯ
+
+            temp1.Add(ref albC.singer, ref albC.song, "Михаил Круг", "Владимирский централ"); // Вызов статического метода расширения способом №1
+            Console.WriteLine($"Длина обновленного расширенного массива с композициями равна: {albC.singer.Length}");
+            Console.WriteLine($"Длина обновленного расширенного массива с исполнителями равна: {albC.song.Length}");
+            Console.WriteLine();
+
+            temp1 = new string[albC.singer.Length];
+
+            StaticClass.Add(temp1, ref albC.singer, ref albC.song, "Ночные снайперы", "31 весна"); // Вызов статического метода расширения способом №2
+            Console.WriteLine($"Длина обновленного расширенного массива с композициями равна: {albC.singer.Length}");
+            Console.WriteLine($"Длина обновленного расширенного массива с исполнителями равна: {albC.song.Length}");
+            Console.WriteLine();
+
+            StaticClass.Add(ref albC.singer, ref albC.song, "Madonna", "You see"); // Вызов статического метода (НЕ РАСШИРЯЮЩЕГО)
+            Console.WriteLine($"Длина обновленного расширенного массива с композициями равна: {albC.singer.Length}");
+            Console.WriteLine($"Длина обновленного расширенного массива с исполнителями равна: {albC.song.Length}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Демонстрация обращения к ранее существовавшему элементу альбома ПО ИНДЕКСУ: {albC[1]}");
+            Console.WriteLine($"Демонстрация обращения к только что добавленному НОВОМУ элементу альбома ПО ИНДЕКСУ: {albC[7]}");
+            Console.WriteLine($"Демонстрация обращения к ранее существовавшему элементу альбома ПО СТРОКЕ: {albC["Бежать"]}");
+            Console.WriteLine($"Демонстрация обращения к только что добавленному НОВОМУ элементу альбома ПО СТРОКЕ: {albC["Владимирский централ"]}");
+            Console.WriteLine();
+        }
+    }
+    class Player
+    {
+        readonly string producer;// Поле только для чтения
+        ushort ram;
+
+        public Player() //Конструктор по умолчанию
+        {
+            this.producer = "SONY";
+        }
+        public Player(ushort r) : this() //Пользовательский конструктор №1
+        {
+            this.Ram = r;
+        }
+        public Player(string p, ushort r) //Пользовательский конструктор №2
+        {
+            this.producer = p;
+            this.Ram = r;
+        }
+        public string Producer //Свойство только для чтения из поля
+        {
+            get
+            {
+                return producer;
+            }
+        }
+        public ushort Ram //Свойство для чтения и записи из поля
+        {
+            get
+            {
+                return ram;
+            }
+            set
+            {
+                if (SetRam(value) == true)
+                    ram = value;
+                else
+                {
+                    Console.WriteLine("Объем памяти д.б. равен одному из следующих значений: 16, 32, 64, 128, 256 или 512 Гб!!");
+                    ram = 16; // присвоение значения по умолчанию
+                }
+            }
+        }
+        // Метод, определяющий  правильность задаваемого обьема памяти
+        public bool SetRam(ushort r)
+        {
+            ushort val = r;
+            if (val == 16 || val == 32 || val == 64 || val == 128 || val == 256 || val == 512)
+                return true;
+            else return false;
+        }
+    }
+    class Album
+    {
+        public string[] singer; //Обьявление массива, содержащего список исполнителей
+        public string[] song; // Обьявление массива, содержащего список песен
+
+        public Album() // Конструктор по умолчанию
+        {
+            singer = this.FillAlbumSinger();
+            song = this.FillAlbumSong();
+        }
+        // Перегрузка конструкторов
+        public Album(string[] si, string[] so) // Пользовательский конструктор
+        {
+            singer = si;
+            song = so;
+        }
+
+        //Перегрузка индексаторов
+        public string this[int index] //Индексатор для доступа к массиву с песнями ПО ЧИСЛУ
+        {
+            get
+            {
+                if ((index < 0) || (index >= this.song.Length)) // при выполнении ХОТЯ БЫ ОДНОГО УСЛОВИЯ нарушаются границы массива и выдается уведомление об этом
+                {
+                    Console.WriteLine("Индекс должен находится в пределах длины АЛЬБОМА!!");
+                    return null;
+                }
+                else
+                    return song[index];
+            }
+            set
+            {
+                song[index] = value;
+            }
+        }
+        public string this[string index] //индексатор для доступа к массиву с исполнителями ПО СТРОКЕ
+        {
+            get
+            {
+                if (index is string)
+                {
+                    switch (index)
+                    {
+                        case "Ласточки": return singer[0]; break;
+                        case "Бежать": return singer[1]; break;
+                        case "Alejandro": return singer[2]; break;
+                        case "Ода нашей любви": return singer[3]; break;
+                        case "Одинокая птица": return singer[4]; break;
+                        case "Владимирский централ": return singer[5]; break;
+                        case "31 весна": return singer[6]; break;
+                        case "You see": return singer[7]; break;
+                        default:
+                            Console.WriteLine("Такой песни нет в альбоме!!");
+                            return null;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Значение индекса д.б. строкового типа!!");
+                    return null;
+                }
+            }
+        }
+        // Закрытый метод для инициализации массива содержащего список исполнителей
+        private string[] FillAlbumSinger()
+        {
+            this.singer = new string[5] { "Ляпис Трубецкой", "Аня Шаркунова", "Lady Gaga", "Т9", "Вячеслав Бутусов" };
+            return this.singer;
+        }
+        // Закрытый метод для инициализации массива содержащего список песен
+        private string[] FillAlbumSong()
+        {
+            this.song = new string[5] { "Ласточки", "Бежать", "Alejandro", "Ода нашей любви", "Одинокая птица" };
+            return this.song;
+        }
+        // Метод для организации коллекции-словаря с наименованиями исполнителей и их песнями соответственно
+        public void CollectionDictionary()
+        {
+            Console.WriteLine("Коллекция-словарь:");
+            for (int i = 0; i < singer.Length; i++)
+            {
+                Console.WriteLine($"Исполнитель: {this[this[i]]} \t  -------> Песня: {this[i]}");
+            }
+        }
+    }
+    // Статический класс с методами расширения
+    public static class StaticClass
+    {
+        //Метод расширения для сортировки массива строк string[]
+        public static void SortStrings(this string[] arr)
+        {
+            Console.WriteLine($"\nМассив строк ДО сортировки:");
+            foreach (string s in arr)
+                Console.Write($"{s}\0");
+            Console.WriteLine();
+
+            Array.Sort(arr);
+
+            Console.WriteLine($"\nМассив строк ПОСЛЕ сортировки:");
+            foreach (string s in arr)
+                Console.Write($"{s}\0");
+            Console.WriteLine();
+        }
+        // Метод для добавления в коллекцию типа Album новых элементов
+        public static void Add(ref string[] singer, ref string[] song, string si, string so)
+        {
+            int lenghtInitial = singer.Length; //локальная переменная хранящая первоначальную длину списка исполнителей/песен
+            Array.Resize(ref singer, singer.Length + 1);
+            singer[lenghtInitial] = si;
+            Console.WriteLine($"Список исполнителей ПОСЛЕ добавления еще одного исполнителя:");
+            for (int i = 0; i < singer.Length; ++i)
+                Console.WriteLine($"{i}--->{singer[i]}");
+
+            Console.WriteLine();
+
+            Array.Resize(ref song, song.Length + 1);
+            song[lenghtInitial] = so;
+            Console.WriteLine($"Список песен ПОСЛЕ добавления еще одной композиции:");
+            for (int i = 0; i < song.Length; ++i)
+                Console.WriteLine($"{i}--->{song[i]}");
+
+            Console.WriteLine();
+        }
+
+        // Перегружаемый метод расширения для добавления в коллекцию типа Album новых элементов
+        public static void Add(this string[] temp, ref string[] singer, ref string[] song, string si, string so)
+        {
+            int lenghtInitial = temp.Length; //локальная переменная хранящая первоначальную длину списка исполнителей/песен
+
+            //Расширение списка исполнителей на одну единицу
+            Array.Resize(ref singer, singer.Length + 1);
+            singer[lenghtInitial] = si;
+            Console.WriteLine($"Список исполнителей ПОСЛЕ добавления еще одного исполнителя:");
+            for (int i = 0; i < singer.Length; ++i)
+                Console.WriteLine($"{i}--->{singer[i]}");
+            Console.WriteLine();
+
+            //Расширение списка песен на одну единицу
+            Array.Resize(ref song, song.Length + 1);
+            song[lenghtInitial] = so;
+            Console.WriteLine($"Список песен ПОСЛЕ добавления еще одной композиции:");
+            for (int i = 0; i < song.Length; ++i)
+                Console.WriteLine($"{i}--->{song[i]}");
+            Console.WriteLine();
+        }
+    }
+}
